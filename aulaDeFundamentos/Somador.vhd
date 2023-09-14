ce628c5217aa47c1797f5d48107d6dd125b53948
Igor <igorponticelli8@gmail.com>
2023-09-14 08:37:09 -0300

    aula_14.09

---
+++ aulaDeFundamentos/Somador.vhd
@@ -0,0 +1,21 @@
+-- Igor Ponticelli
+-- igor.ponticelli@edu.pucrs.br
+
+library ieee;
+    use ieee.std_logic_1164.all;
+
+entity somador_3b is
+port
+(
+    op_a    :   in std_logic_vector(2 downto 0);
+    op_b    :   in std_logic_vector(2 downto 0);
+    soma    :   in std_logic_vector(3 downto 0)
+);
+end somador_3b;
+
+architecture soma_maneira of somador_3b is
+
+begin
+
+
+end soma_maneira;

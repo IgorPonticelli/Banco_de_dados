4268578a1f22095ddc84debea1c6850b0582ad29
Igor <igorponticelli8@gmail.com>
2023-09-14 09:08:28 -0300

    somadorCompleto

--- aulaDeFundamentos/Somador.vhd
+++ aulaDeFundamentos/Somador.vhd
@@ -15,7 +15,40 @@ end somador_3b;
 
 architecture soma_maneira of somador_3b is
 
-begin
+    signal coi :    std_logic_vector(2 downto 1);
 
+begin
+    --Instanciação do meio_somador #0
+    ha : entity work.halhAdder
+    port map
+    (
+        a   => op_a(0), 
+        b   => op_b(0),
+        s   => soma(0),
+        co  => coi(0)
+    );
+
+    --Instanciação do somador_completo #0
+    fa0: entity work.full_adder
+    port map
+    (
+        a   => op_a(1),
+        b   => op_b(1),
+        ci  => coi(1),
+        s   => soma(1),
+        co  => coi(2)
+    );
+
+    --Instanciação do somador_completo #1
+    fa1 entity work.full_adder
+    port map
+    (
+        a   => op_a(2),
+        b   => op_b(2),
+        ci  => coi(2),
+        s   => soma(2),
+        co  => coi(3)
+
+    );
 
 end soma_maneira;
